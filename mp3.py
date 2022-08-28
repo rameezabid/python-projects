6b5e494b6c7c1405be64644d44636b8bd415774d
Rameez Abid <110272441+rameezabid@users.noreply.github.com>
2022-08-28 06:55:18 -0400

    Add files via upload

---
+++ mp3.py
@@ -0,0 +1,20 @@
+import youtube_dl
+def run():
+    video_url = input("YouTube Video URL: ")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'worstaudio/worst',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+if __name__=='__main__':
+    run()
---
+++ qr.py
@@ -0,0 +1,8 @@
+import os
+import qrcode
+
+data = input('Enter Data to Covert to QR: ')
+fileName = input('Save File As: ')
+img = qrcode.make(data)
+img.save(fileName + ".png", "PNG")
+os.system("open " + fileName + ".png")
